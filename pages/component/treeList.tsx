c8cd30db8ad0e07f6e7d693ab69a6f08fa9ca9db
akuaki96 <akuaki96@outlook.jp>
2023-03-23 05:16:28 +0900

    feat/ホーム画面のレイアウト調整

--- pages/component/treeList.tsx
+++ pages/component/treeList.tsx
@@ -1,7 +1,54 @@
-export const TreeList = () => {
+import style from "../../styles/TreeList.module.css";
+import { FC } from "react";
+import { useRecoilState } from "recoil";
+import { treeListState } from ".././component/atoms";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+
+type Props = {
+  setSelected: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const TreeList: FC<Props> = (props) => {
+  const { setSelected } = props;
+
+  const [treeList, setTreeList] = useRecoilState(treeListState);
   return (
-    <div>
-      <div></div>
-    </div>
+    <ul className={style.ul}>
+      {treeList.map((value, index) => (
+        <li
+          key={index}
+          className={style.listContent}
+          onClick={() => {
+            setSelected(index);
+          }}
+        >
+          <div className={style.textContent}>
+            <p className={style.productNameText}>{value.productName}</p>
+            <p className={style.subText}>
+              <span>要素数 : </span>
+              {value.featureList.length}
+            </p>
+          </div>
+
+          <div className={style.buttonContent}>
+            <button className={style.button}>
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                className={style.icon}
+              ></FontAwesomeIcon>
+            </button>
+
+            <button className={style.button}>
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                className={style.icon}
+              ></FontAwesomeIcon>
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@ import { Header } from "./component/header";
 import style from "../styles/Home.module.css";
 import { SideBar } from "./component/sideBar";
 import { useState } from "react";
-import { featureListState } from "./component/atoms";
 import { useRecoilState } from "recoil";
 import { treeListState } from "./component/atoms";
+import { TreeList } from "./component/treeList";
 
 const Home = () => {
   const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
 
   const [treeList, setTreeList] = useRecoilState(treeListState);
+
+  const [selected, setSelected] = useState<number>(0);
 
   return (
     <div className={style.homeBody}>
@@ -24,21 +26,19 @@ const Home = () => {
         </div>
 
         <div className={style.treeListArea}>
-          <ul>
-            {treeList.map((treeList, index) => (
-              <li key={index} className={style.treeListContents}>
-                <p>{treeList.productName}</p>
-                <ul>
-                  {treeList.featureList.map((feat, featIndex) => (
-                    <li key={featIndex}>
-                      <p>{feat.name}</p>
-                      <ul>
-                        {feat.techList.map((tech, techIndex) => (
-                          <li key={techIndex}>
-                            <p>{tech}</p>
-                          </li>
-                        ))}
-                      </ul>
+          <TreeList setSelected={setSelected}></TreeList>
+        </div>
+
+        <div className={style.firstInputArea}>
+          <p>{treeList[selected].productName}</p>
+          <ul className={style.ul}>
+            {treeList[selected].featureList.map((feat, featIndex) => (
+              <li key={featIndex}>
+                <p>{feat.name}</p>
+                <ul className={style.ul}>
+                  {feat.techList.map((tech, techIndex) => (
+                    <li key={techIndex}>
+                      <p>{tech}</p>
                     </li>
                   ))}
                 </ul>
@@ -46,7 +46,6 @@ const Home = () => {
             ))}
           </ul>
         </div>
-        <div className={style.firstInputArea}></div>
       </div>
       {/* <FirstInput></FirstInput> */}
     </div>
